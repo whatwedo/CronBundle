4b88b257cd0577c9129b1fc8ea5e6fbe25aca895
Timo Bühlmann <timo@whatwedo.ch>
2021-04-14 11:35:21 +0200

    fix: compatibility with symfony command

--- CronJob/CronJobInterface.php
+++ CronJob/CronJobInterface.php
@@ -39,7 +39,7 @@ interface CronJobInterface
      *
      * @return string
      */
-    public function getDescription(): ?string;
+    public function getDescription();
 
     /**
      * Returns command name
