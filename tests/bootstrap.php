b1d9964308cee9481333a8d52c28c05c7aa264f7
Robin Lehrmann <robin@whatwedo.ch>
2025-03-18 17:35:21 +0100

    ci: recreate the database on ci for tests
    Signed-off-by: Robin Lehrmann <robin@whatwedo.ch>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,6 @@ require \dirname(__DIR__).'/vendor/autoload.php';
 
 (new Filesystem())->remove(__DIR__.'/../var');
 
-shell_exec('tests/App/bin/console doctrine:database:create --if-not-exists -n --env=test');
-shell_exec('tests/App/bin/console doctrine:schema:drop --full-database --force --env=test');
-shell_exec('tests/App/bin/console doctrine:schema:update --force --env=test');
+shell_exec('tests/App/bin/console doctrine:database:drop --if-exists --force -n --env=test');
+shell_exec('tests/App/bin/console doctrine:database:create -n --env=test');
+shell_exec('tests/App/bin/console doctrine:schema:create --env=test');
